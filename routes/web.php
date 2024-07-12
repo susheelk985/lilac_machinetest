c10dc6618dd45bb5f7d279c55afaacb3ba0e38a8
SUSHEEL KUMAR R <susheelk985@gmail.com>
2024-07-12 13:20:39 +0530

    some unwanted routes removed

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,4 @@
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
-Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/', SearchController::class);
